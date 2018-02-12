dcb0a9b16b649c99455bdf6015040391852c903e
Koichi Shiraishi <zchee.io@gmail.com>
2018-02-12 22:11:56 +0900

    all: add doc.go to some packages

---
+++ parser/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2018 The go-vis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package parser parses Go packages and convert Graphviz dot file syntax.
+package parser
